492a6b3c5b6ef8e200f587c91ecff2365e32b454
caelebn <cjnasoff@gmail.com>
2019-02-18 14:03:29 -0500

    Update

--- lib/bubbles.dart
+++ lib/bubbles.dart
@@ -20,7 +20,7 @@ class MyGrid extends State<EntryPage>{
   MyGrid(int _widthTiles, int _heightTiles){
     this._widthTiles = _widthTiles;
     this._heightTiles = _heightTiles;
-    this._myList = new List(_widthTiles);
+    this._myList = genBubbles(_widthTiles);
   }
   
   int getHeightTiles(){
@@ -31,36 +31,61 @@ class MyGrid extends State<EntryPage>{
     return this._widthTiles;
   }
 
-  List<Widget> _generateList(BuildContext context){
-    _myList[0] = new Bubble('Pop!', 200, true).build(context);
-    return _myList;
+  Widget buildGrid(int crossCount, double padding, double spacing){
+    List<Widget> _list = genBubbles(crossCount);
+    return new GridView.count(
+      primary: false,
+      padding: EdgeInsets.all(padding),
+      crossAxisSpacing: spacing,
+      crossAxisCount: crossCount,
+      children: _list,
+    );
   }
 
-  Widget makeBubble(Bubble b){
-    return new Opacity(opacity: b.getPressed() ? 1.0 : 0.0,
-        child: FloatingActionButton(
-          backgroundColor: Colors.blueAccent[300],
-          child: Text(b.getEntry()),
-          onPressed: ((){
-            setState((){
-//              print('' + b.getPressed().toString());
-              b.changePressed();
-//              print('' + b.getPressed().toString());
-            });
-          })
+  List<Widget> genBubbles(int size){
+    List<Widget> _list = List<Widget>(size);
+    for (int i = 0; i < size; i++){
+      _list[i] = makeBubble(new Bubble(i.toString(), (i+1)*10.0, true));
+    }
+    return _list;
+  }
+
+  Widget makeBubble(Bubble b) {
+    print('Visibility' + b.getPressed().toString());
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      child: Center(
+        child: Container(
+          width: b.getPriority(),
+          height: b.getPriority(),
+          child: Opacity(
+              opacity: b.getPressed() ? 1.0 : 0.0,
+              child: FloatingActionButton(
+                  backgroundColor: Colors.blueAccent[300],
+                  child: Text(b.getEntry()),
+                  onPressed: () {
+                    setState(() {
+                      print(b.getPressed().toString());
+                      b.changePressed();
+                      print(b.getPressed().toString());
+                    });
+                  }
+              )
+          )
         )
+      ),
     );
   }
   Bubble b1 = new Bubble('Pop!', 300, true);
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BUBL'),
-    ),
-    body: makeBubble(b1),
-    backgroundColor: Colors.amber[100],
-    );
+        appBar: AppBar(
+          title: Text('BUBL'),
+        ),
+        body: buildGrid(5, 5, 10),
+        );
   }
 }
 
@@ -89,6 +114,10 @@ class Bubble extends State<EntryPage>{
   double getPriority(){
     return this._priority;
   }
+  void setPriority(double newPri){
+    this._priority = newPri;
+  }
+
   @override
   Widget build(BuildContext context){
     return new Opacity(opacity: _pressed? 1.0 : 0.0,
