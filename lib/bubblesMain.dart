e158bb9c60c837886d3d14bb0439d8244d048701
caelebn <cjnasoff@gmail.com>
2019-03-12 13:42:11 -0400

    Updated bubble widgets to be based off of screen size. Fixed bug so that the bubbles will now disappear.

--- lib/bubblesMain.dart
+++ lib/bubblesMain.dart
@@ -18,112 +18,110 @@ class MyGrid extends State<EntryPage>{
   int _maxWidthTiles; //Maximum number of tiles to be held on the screen across the width
   int _maxHeightTiles; //Maximum number of tiles to be held on the screen across the height
   BubblesList _bList; //List of bubbles
-  double _tileWidth; //width of a given tile
-  double _tileHeight; //height of a given tile
-  List<Widget> _myList; //List of physical widgets
-  Widget _grid;
 
   MyGrid(int _maxWidthTiles, int _maxHeightTiles){
-    this._bList = populateBList(5); //TODO: Make this not for testing/prototype 1
+    //Initialize everything
+    this._bList = new BubblesList();
     this._maxWidthTiles =_maxWidthTiles;
     this._maxHeightTiles =_maxHeightTiles;
-    _tileWidth = 0.0; //Initialized to 0, will set in makeBubble
-    _tileHeight = 0.0; //Initialized to 0, will set in makeBubble
-    _myList = new List<Widget>();
-    _grid = makeGrid();
   }
 
-  //Widget creation instructions for a bubble widget based on Bubble b
-  Widget makeBubble(Bubble b, BuildContext context){
+  @override
+  void initState(){
+    this._bList = populateBList(3); //populate the bubblesList on start
+  }
+
+  //FOR TESTING/PROTOTYPE 1 ONLY
+  //Used to populate _bList with Bubble objects
+  BubblesList populateBList(int howMany){
+    BubblesList nList = new BubblesList();
+    for (int i = 0; i < howMany; i++){
+      nList.addBubble(new Bubble(i.toString(), ((i+1)*33)/100.0, true, 0, i)); //100.0 is used to convert to percent
+    }
+    return nList;
+  }
+
+  //creates a grid of widgets given a list of Bubble objects
+  Widget makeGrid(BubblesList bL, double padding, double spacing, int crossCount, BuildContext context){//BuildContext context){
     double _screenWidth = MediaQuery.of(context).size.width; //width of the screen
     double _screenHeight = MediaQuery.of(context).size.height; //height of the screen
 
     double _pW = _screenWidth / _maxWidthTiles; //PlaceHolder Width
     double _pH = _screenHeight / _maxHeightTiles; //PlaceHolder Height
 
+    print("_pW = " + _pW.toString());
+    print("_pH = " + _pH.toString());
+
     double _tileSize = min(_pW, _pH); //set the tile size to the smallest placeholder, will be used to form a square Container
+    
+    print("I chose: " + _tileSize.toString());
 
-    double _bubbleSize = b.getPriority() * _tileSize;
-
-    // bool vis = b.getPressed();
-    // print("Vis is set to " + vis.toString());
-    return Container(
-        width: _tileSize,
-        height: _tileSize,
+    List<Widget> _widgetList = []; //Used to read the bubbles into
+    //goes through BubblesList param and creates a widget list based off of it
+    for (int i = 0; i < bL.getSize(); i++) {
+      double _currPrior = bL.getBubbleAt(i).getPriority();
+      double _size = _currPrior * _tileSize; //sets the size that the widget will be based off of screen size
+      _widgetList.add(new Container(
+        width: _size,
+        height: _size,
         child: Center(
-          child: Opacity(
-            opacity: b.getPressed() ? 1.0 : 0.0,
+          
             child: Container(
-              width: _bubbleSize,
-              height:_bubbleSize,
-                child:InkResponse(
-                  onTap: (){
-                    setState((){ //TODO: FIX THIS PLEASE SOMEONE
-                      print(b.getPressed().toString());
-                      b.changePressed();
-                      print(b.getPressed().toString());
-                    });
-                  },
-                  onLongPress: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BubbleDescription(presscount: b.getNumPressed(),)),
-                    );
-                  },
-                  child: new Container(
-                    width: _bubbleSize,
-                    height:_bubbleSize,
-                    decoration: new BoxDecoration(
-                      color: Colors.blue,
-                      border: new Border.all(color:Colors.white, width: _bubbleSize),
-                      borderRadius: new BorderRadius.circular(_bubbleSize),
+              
+                width: _size,
+                height: _size,
+                child: Opacity(
+                    opacity: _bList.getBubbleAt(i).getPressed() ? 1.0 : 0.0,
+                    child: InkResponse(
+                      
+                      onTap: () {                         
+                          setState(() {
+                            print(_bList.getBubbleAt(i).getPressed().toString());
+                            _bList.changeElementPressed(i);
+                            print(_bList.getBubbleAt(i).getPressed().toString());
+                            print(_bList.getBubbleAt(i).getNumPressed().toString());
+                          });
+                        },
+                        onLongPress: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => BubbleDescription(presscount:_bList.getBubbleAt(i).getNumPressed())),
+                        );
+                      },
+
+                      child: Container(
+                        width: _size,
+                        height: _size,
+                        decoration: new BoxDecoration(
+                          color: Colors.blue,
+                          border: new Border.all(color: Colors.white, width: _size),
+                          borderRadius: new BorderRadius.circular(_size),
+                        ),
+                        child: Text(_bList.getBubbleAt(i).getEntry(), textAlign: TextAlign.center,)
+                      ),
                     ),
-                  ),
                 ),
-              ),
+            
             ),
-          )
-    );
-  }
-
-
-
-  //FOR TESTING/PROTOTYPE 1 ONLY
-  //Used to populate _bList
-  BubblesList populateBList(int howMany){
-    BubblesList nList = new BubblesList();
-    for (int i = 0; i < howMany; i++){
-      nList.addBubble(new Bubble(i.toString(), (i*10.0)/100.0, true, 0, i));
+        ),
+      ));
     }
-    return nList;
-  }
-
-  //FOR TESTING/PROTOTYPE 1 ONLY
-  //creates a list of widgets with bubbles
-  void makeBubbles(BuildContext context){
-    for (int i = 0; i < this._bList.getSize(); i++) {
-      _myList.add(makeBubble(this._bList.getBubbleAt(i), context));
-    }
-  }
-
-  //Specifies how to build grid
-  //TODO: Remove magic numbers
-  Widget makeGrid(){
-    return new GridView.count(
-      primary: false,
-      padding: EdgeInsets.all(5.0),
-      crossAxisSpacing: 5.0,
-      crossAxisCount: 5,
-      children: _myList,
-    );
-  }
+      //returns the gridview of the widget list
+      return new GridView.count(
+        primary: false,
+        padding: EdgeInsets.all(padding),
+        mainAxisSpacing: spacing,
+        crossAxisSpacing: spacing,
+        crossAxisCount: crossCount,
+        children: _widgetList,
+      );
+  }    
   Widget build(BuildContext context) {
-    makeBubbles(context);
     return Scaffold(
         appBar: AppBar(
-          title: Text('BUBL'),
+          title: Text('Bubble'),
         ),
-        body: makeGrid()
+        body: makeGrid(_bList, 0.5, 0.5, 3, context),
         );
   }
 }
