ce7af7aa0bec63cbe20c3a7d1b700486c287b519
MartinPrice141 <pricem2@students.rowan.edu>
2019-03-31 17:26:29 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,120 @@ import 'database.dart';
 import 'bubbles.dart';
 
 
-void main() => runApp(BubbleView());
+void main() => runApp(DBview());
 
+
+class DBview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SQFlite Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  final db = DB.instance;
+  //Bubble b1 = new Bubble.defaultBubble();
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BUBL Test DB'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('insert values', style: TextStyle(fontSize: 20),),
+              onPressed: () { _insert(); },
+            ),
+            RaisedButton(
+              child: Text('Refresh', style: TextStyle(fontSize: 20),),
+              onPressed: () { _refresh(); },
+            ),
+            RaisedButton(
+              child: Text('Test Attribute', style: TextStyle(fontSize: 20),),
+              onPressed: () { _testDB(); },
+            ),
+            //BubbleWidget(bubble: new Bubble.defaultBubble()),
+          ],
+        ),
+      ),
+      //body:
+      //new Stack(
+      //children: [BubbleWidget(bubble: b1),
+      //BubbleWidget(bubble: b2)
+    );
+  }
+
+  //Helper methods used by Martin to quickly test the DB
+  void _queryBub() async
+  {
+    final allBubbles = await db.queryBubble();
+    print("Printing Bubbles");
+    try{ allBubbles.forEach((row) => print(row)); }
+    catch (e) { print("No bubs"); }
+
+  }
+  void _queryPop() async
+  {
+    final allPop = await db.queryBubblesForRePop();
+    print("Printing popped");
+    try{ allPop.forEach((row) => print(row)); }
+    catch (e) { print("No pops"); }
+  }
+  void _insert() async
+  {
+    //print("Testing the bubble table");
+    //print(await db.insertBubble('Hello World 2', 'Test to See the DB 2', 100, 200, 100, 0.50, 5, 0.75, 0.25));
+    //print("Testing the pop table");
+    //print(await(db.insertPop()));
+
+    print("Creating a new bubble");
+    await db.insertBubble("Hello", "There", 120, 140, 160, 0.50, 2, 0.75, 0.25);
+    int x = await db.queryLastCreatedBubbleID();
+    Map<String, dynamic> y = await db.queryBubbleByID(x, []);
+    Bubble b2 = new Bubble.BubbleFromDatabase(x,y['title'],y['description'],
+                  new Color.fromRGBO(y['color_red'],y['color_green'],
+                                     y['color_blue'],y['opacity']),
+                  y['size'], y['xPos'], y['yPos'], y['opacity'], y['times_popped']);
+    print("Bubble created from DB");
+    print(b2.getBubbleID());
+    print(b2.getDescription());
+    print(b2.getSize());
+
+  }
+  void _testDB() async
+  {
+    print('Printing out bubl table:');
+    final bubl = await db.queryBubble();
+    try{ bubl.forEach((row) => print(row)); }
+    catch(e) {print(e); }
+
+    print('Printing out pop_record table:');
+    final pop = await db.queryPop();
+    try{ pop.forEach((row) => print(row)); }
+    catch(e) {print(e); }
+  }
+  void _refresh() async
+  {
+    print("Refreshing Bubl.db");
+    await db.refreshDB();
+  }
+
+  void _testBubble() async
+  {
+    print("Testing a Bubble");
+    //print(b1.getBubbleID());
+  }
+}
 
 class BubbleApp extends StatefulWidget{
   @override
@@ -14,56 +126,38 @@ class BubbleApp extends StatefulWidget{
 
 class BubbleAppState extends State<BubbleApp>{
   Bubble b1 = new Bubble.defaultBubble();
-  Bubble b2 = new Bubble("DOUG DIMMADOME", 
-                        "OWNER OF THE DIMSDALE DIMMADOME",
-                        Colors.red,
-                        200.0,
-                        true,
-                        0.2,
-                        0.2,
-                        1.0
-    );
-    // Test the database
-    final db = DB.instance;
+  Bubble b2 = new Bubble("DOUG DIMMADOME",
+      "OWNER OF THE DIMSDALE DIMMADOME",
+      Colors.red,
+      200,
+      true,
+      0.2,
+      0.2,
+      1.0
+  );
   //Implement move bubble here
 
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BUBL'),
-      ),
-      body: new Stack(
-        children: [BubbleWidget(bubble: b1),
-                  BubbleWidget(bubble: b2), 
-                  //RaisedButton(child: Text("Query DB for Bubls", style: TextStyle(fontSize: 20),),
-                  //onPressed: () {_searchDB();},),
-                  RaisedButton(child: Text("Update Bubl DB", style: TextStyle(fontSize: 20),),
-                  onPressed: () {_updateDB();},)],
-      )
+        appBar: AppBar(
+          title: Text('BUBL'),
+        ),
+        body: new Stack(
+          children: [BubbleWidget(bubble: b1),
+          BubbleWidget(bubble: b2),],
+        )
     );
   }
-
-  /**
-   * Heleper methods used by Martin to quickly test the DB
-   */
-  void _searchDB() async
-  {
-
-  }
-  void _updateDB() async
-  {
-    await db.enterBubbleUpdate('Hello World', 'Test to See the DB', "blue", 5, 0.75, 0.25);
-  }
-
-
 }
 
+/*
 class BubbleView extends StatelessWidget {
-    @override
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
       title:'App4 with DB',
-      home: BubbleApp(), 
+      home: BubbleApp(),
     );
   }
 }
+*/
