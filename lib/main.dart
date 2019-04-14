90d25d894495fe5b21780a08cf9aeef17cce9795
MartinPrice141 <pricem2@students.rowan.edu>
2019-04-13 21:56:17 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'database.dart';
-import 'bubbles.dart';
 
 
 void main() => runApp(DBview());
@@ -33,7 +32,7 @@ class MyHomePage extends StatelessWidget{
           children: <Widget>[
             RaisedButton(
               child: Text('insert values', style: TextStyle(fontSize: 20),),
-              onPressed: () { _insert(); },
+              onPressed: () { _testDB(); },
             ),
             RaisedButton(
               child: Text('Refresh', style: TextStyle(fontSize: 20),),
@@ -41,20 +40,15 @@ class MyHomePage extends StatelessWidget{
             ),
             RaisedButton(
               child: Text('Test Attribute', style: TextStyle(fontSize: 20),),
-              onPressed: () { _testColor(); },
+              onPressed: () { _queryPop(); },
             ),
             RaisedButton(
               child: Text('login', style: TextStyle(fontSize: 20),),
               onPressed: () { _login(); },
             ),
-            //BubbleWidget(bubble: new Bubble.defaultBubble()),
           ],
         ),
       ),
-      //body:
-      //new Stack(
-      //children: [BubbleWidget(bubble: b1),
-      //BubbleWidget(bubble: b2)
     );
   }
 
@@ -69,31 +63,13 @@ class MyHomePage extends StatelessWidget{
   }
   void _queryPop() async
   {
-    final allPop = await db.queryBubblesForRePop();
+    final allPop = await db.queryPop();
     print("Printing popped");
     try{ allPop.forEach((row) => print(row)); }
     catch (e) { print("No pops"); }
   }
   void _insert() async
   {
-    //print("Testing the bubble table");
-    //print(await db.insertBubble('Hello World 2', 'Test to See the DB 2', 100, 200, 100, 0.50, 5, 0.75, 0.25));
-    //print("Testing the pop table");
-    //print(await(db.insertPop()));
-
-    print("Creating a new bubble");
-    await db.insertBubble("Hello", "There", 120, 140, 160, 0.50, 2, 0.75, 0.25);
-    int x = await db.queryLastCreatedBubbleID();
-    Map<String, dynamic> y = await db.queryBubbleByID(x, []);
-    Bubble b2 = new Bubble.BubbleFromDatabase(x,y['title'],y['description'],
-                  new Color.fromRGBO(y['color_red'],y['color_green'],
-                                     y['color_blue'],y['opacity']),
-                  y['size'], y['xPos'], y['yPos'], y['opacity'], y['times_popped']);
-    print("Bubble created from DB");
-    print(b2.getBubbleID());
-    print(b2.getDescription());
-    print(b2.getSize());
-
   }
   void _testDB() async
   {
@@ -116,7 +92,6 @@ class MyHomePage extends StatelessWidget{
   void _testBubble() async
   {
     print("Testing a Bubble");
-    //print(b1.getBubbleID());
   }
 
   void _login() async
@@ -135,34 +110,3 @@ class MyHomePage extends StatelessWidget{
 
   }
 }
-
-/*class BubbleApp extends StatefulWidget{
-  @override
-  BubbleAppState createState() => BubbleAppState();
-}
-
-class BubbleAppState extends State<BubbleApp>{
-  Bubble b1 = new Bubble.defaultBubble();
-  Bubble b2 = new Bubble("DOUG DIMMADOME",
-      "OWNER OF THE DIMSDALE DIMMADOME",
-      Colors.red,
-      200,
-      true,
-      0.2,
-      0.2,
-      1.0
-  );
-  //Implement move bubble here
-
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('BUBL'),
-        ),
-        body: new Stack(
-          children: [BubbleWidget(bubble: b1),
-          BubbleWidget(bubble: b2),],
-        )
-    );
-  }
-}*/
