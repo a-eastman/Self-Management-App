9984169227aa7efa3a9c55b82cbf5e0f16f40ec4
Brian Intile <brianintile@gmail.com>
2019-04-19 16:19:20 -0400

    Swap main and main_two

--- lib/main_two.dart
+++ lib/main_two.dart
@@ -1,104 +1,63 @@
+
 import 'package:flutter/material.dart';
-import 'bubble_widget.dart';
-//import 'iamthebubble.dart';
-import 'list_widget.dart';
-import 'bubbles.dart';
-import 'themeSelection.dart';
-import 'themes.dart';
 import 'database.dart';
-import 'settingsScreen.dart';
 
-final db = DB.instance;
-void main() => runApp(BubbleView());
 
-class BubbleView extends StatelessWidget {
-  bool newDay;
+void main() => runApp(DBview());
+  final db = DB.instance;
+
+class DBview extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    final BubbleTheme theme =BubbleTheme();
-    login();
-    BubblesList _bList;
-    if(newDay == true) {_bList = new BubblesList(); print('New Day'); }    // new day, fresh list
-    else {_bList = new BubblesList.unpoppedBubbles(); print('Welcome Back');}// same day
-
-    return StreamBuilder<ThemeData>(
-      initialData: theme.buildBubbleTheme().data,
-      stream: theme.themeDataStream,
-      builder: (BuildContext context, AsyncSnapshot<ThemeData> snapshot) {
-        return MaterialApp(
-          title: 'Bubl with DB Integrated',
-          theme: snapshot.data,
-          home: BubbleApp(
-            theme: theme,
-            bList: _bList,
-          ),
-        );
-      },
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SQFlite Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
-  
-  ///determines whether it is a new day
-  void login() async
-  { newDay = await db.login(); }
 }
 
-// ignore: must_be_immutable
-class BubbleApp extends StatefulWidget{
-  final BubbleTheme theme;
-  final Color globalBubbleColor;
-  BubblesList bList;
-  List<BubbleWidget> _widList = [];
-  
-  BubbleApp({Key key, this.theme, this.globalBubbleColor, this.bList});
+class MyHomePage extends StatelessWidget{
   @override
-  BubbleAppState createState() =>
-      BubbleAppState(bList, _widList, theme, globalBubbleColor);
-}
-
-class BubbleAppState extends State<BubbleApp>{
-  BubbleTheme _theme;
-  Color globalBubbleColor;
-  List<BubbleWidget> _myList;
-  BubblesList _bList;
-  BubbleAppState(BubblesList _bList, List<BubbleWidget> _widList,
-      this._theme, this.globalBubbleColor){
-    this._bList =_bList;
-    this._myList = _widList;
-  }
-
-  void setBubbleColor(Color newBubbleColor){
-    this.globalBubbleColor = newBubbleColor;
-  }
-
-  @override
-  void initState(){
-    super.initState();
-    //setState(() {build(context);} );
-    _myList = [];
-    //_bList = new BubblesList();
-  }
-
-  ListWidget _buildListView(){
-    return new ListWidget(_bList, _theme);
-  }
-
-  Widget _buildBubbleView(){
-    return new BubbleWidget(_bList, _theme);
-  }
-
-  Widget _buildSettingsScreen() {
-    return new SettingsScreen(_bList, _theme);
-  }
-
   Widget build(BuildContext context){
-    return PageView(
-      children: <Widget>[
-        _buildSettingsScreen(),
-        _buildBubbleView(),
-        _buildListView(),
-      ],
-      controller: PageController(initialPage: 1),
-      pageSnapping: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BUBL Test DB'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Refresh', style: TextStyle(fontSize: 20),),
+              onPressed: () { _refresh(); },
+            ),
+            RaisedButton(
+              child: Text('login', style: TextStyle(fontSize: 20),),
+              onPressed: () { _login(); },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+  void _refresh() async
+  {
+    print("Refreshing Bubl.db");
+    await db.refreshDB();
+    print('DB refreshed');
+  }
+
+  void _login() async
+  {
+    print('Logging in now');
+    final newDay = await db.login();
+    if(newDay) print('Logged in to a new day'); 
+    else print('Welcome back!');
+    final logins = await db.queryAppState();
+    try{logins.forEach((row) => print(row)); }
+    catch(e) {print(e); print('No logins found'); }
   }
 }
