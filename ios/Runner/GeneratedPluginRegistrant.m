2651d16833dd9655f4d87b37b4bbfa51968a872f
MartinPrice141 <pricem2@students.rowan.edu>
2019-04-19 14:38:37 -0400

    Add files via upload

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <audioplayer/AudioplayerPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <sqflite/SqflitePlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [AudioplayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
 }
 
 @end
